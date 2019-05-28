0b9154a732f0607d755d085655d439f5bb1d3fb0
Kerem <keremgocen@gmail.com>
2019-05-28 17:25:06 +0300

    rename sptest to baseapp

--- baseapp/wsgi.py
+++ baseapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sptest project.
+WSGI config for baseapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sptest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseapp.settings')
 
 application = get_wsgi_application()
